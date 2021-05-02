491b995ccbebe608a69c67ed2c0aa1a78c70f2d0
LucasHaug <lucas.haug@hotmail.com>
2021-05-02 16:21:10 -0300

    ✨ Add basic install script

---
+++ basic_install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+GITHUB_USER="user"
+GITHUB_EMAIL="email@email.com"
+
+# Update amd upgrade before starting
+sudo apt update
+sudo apt upgrade -y
+
+# Install basic tools
+sudo apt install gcc build-essential make git python3 python3-pip -y
+
+# Config git
+git config --global init.defaultBranch main
+git config --global core.autocrlf input
+git config --global user.name "$GITHUB_USER"
+git config --global user.email "$GITHUB_EMAIL"
+
+# Install fish and related
+sudo apt install fish -y
+
+chsh -s $(which fish)
+
+sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+printf "starship init fish | source\r\n\n" >> ~/.config/fish/config.fish
+
+yes | curl -L https://get.oh-my.fish | fish
+
+fish -c 'omf install bass -y'
+
+printf "set -x LS_COLORS $LS_COLORS ':ow=01;35'\r\n\n" >> ~/.config/fish/config.fish
+printf "set -l USER_PROGRAMS_FOLDER /home/lhaug/room/UserPrograms\r\n\n" >> ~/.config/fish/config.fish

b88bba04d8046ea9579ce0712eecbe8a317a3663
LucasHaug <lucas.haug@hotmail.com>
2021-05-02 16:22:17 -0300

    ✨ Add git ssh config script

---
+++ git_ssh_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+GITHUB_EMAIL="email@email.com"
+
+# Generate new key
+ssh-keygen -t ed25519 -C "$GITHUB_EMAIL"
+
+# Add key to ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+# Copy key
+sudo apt install xclip -y
+xclip -selection clipboard < ~/.ssh/id_ed25519.pub

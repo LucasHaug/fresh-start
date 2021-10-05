f4219a794ab9f42497732a7716d43f1d4a6976eb
LucasHaug <lucas.haug@hotmail.com>
2021-10-05 14:38:56 -0300

    🔥 Remove unnecessary install

--- basic_install.sh
+++ basic_install.sh
@@ -17,7 +17,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install basic tools
-sudo apt install gcc build-essential make git python3 python3-pip -y
+sudo apt install build-essential git python3 python3-pip -y
 
 # Config git
 git config --global init.defaultBranch main

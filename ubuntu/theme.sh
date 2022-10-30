788a7a1c802bc6a59a9f087a0b48744fd151074e
LucasHaug <lucas.haug@hotmail.com>
2022-10-30 19:21:40 +0100

    ♻️ Update theme install

--- ubuntu/theme.sh
+++ ubuntu/theme.sh
@@ -1,33 +1,42 @@
 #!/usr/bin/env bash
 
-# Get Dracula
+# Get Dracula for gtk4
+wget https://github.com/odziom91/libadwaita-themes/archive/refs/heads/main.zip
+
+unzip main.zip -d libadwaita-themes
+
+cd libadwaita-themes/libadwaita-themes-main/dracula
+
+unzip dracula-v42.zip -d dracula
+
+mv dracula/gtk-4.0/* ~/.config/gtk-4.0
+
+cd ../../..
+
+rm -rf libadwaita-themes
+rm main.zip
+
+# Get Dracula for gtk3 and gtk2
 wget https://github.com/dracula/gtk/archive/master.zip
 
 unzip master.zip -d Dracula
 
-cd Dracula
-mv gtk-master/* .
-rm -rf gtk-master
+cd Dracula/gtk-master
 
-cd ..
+mv gtk-3.0/ ~/.config/gtk-3.0
+mv gtk-2.0/ ~/.config/gtk-2.0
+
+cd ../..
 
 rm master.zip
-
-mkdir -p ~/.themes
-mv Dracula ~/.themes
-
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+rm -rf Dracula
 
 # Get icons
-sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:papirus/papirus
 sudo apt update
-sudo apt install numix-icon-theme-circle
+sudo apt install papirus-icon-theme
 
-gsettings set org.gnome.desktop.interface icon-theme 'Numix-Circle'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
 # Install tweaks
-sudo apt install gnome-tweak
-
-# @todo Do the following using the command line
-echo "Install the Gnome Extension: User Themes and in 'Appearance' change the shell theme to Dracula"
+sudo apt install gnome-tweaks

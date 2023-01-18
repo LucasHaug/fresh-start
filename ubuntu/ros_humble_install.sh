069fb9188d6aa8a4c1352ec3c6a6ee640fa268eb
LucasHaug <lucas.haug@hotmail.com>
2023-01-18 19:52:47 +0100

    ✨ Add rviz and rqt desktop entries

--- ubuntu/ros_humble_install.sh
+++ ubuntu/ros_humble_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 WORKING_DIR=$(pwd)
+ROS_DISTRO=humble
 
 # Set locale
 locale  # check for UTF-8
@@ -25,9 +26,38 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update
 sudo apt upgrade
 
-sudo apt install ros-humble-desktop-full -y
+sudo apt install ros-${ROS_DISTRO}-desktop-full -y
 
 # Setup
-printf "bass source /opt/ros/humble/setup.bash\n\n" >> ~/.config/fish/config.fish
+printf "bass source /opt/ros/${ROS_DISTRO}/setup.bash\n\n" >> ~/.config/fish/config.fish
 
 cp "$WORKING_DIR"/.config/fish/conf.d/ros2.autosource.fish ~/.config/fish/conf.d/
+
+# RViz2 and RQt desktop entries
+cat <<EOF > $HOME/.local/share/applications/rviz2.desktop
+[Desktop Entry]
+Name=RViz2
+GenericName=RViz2
+Comment=3D visualization tool for ROS 2
+Exec=rviz2
+Icon=/opt/ros/${ROS_DISTRO}/share/rviz_common/icons/package.png
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+StartupNotify=true
+Categories=Development;Robotics;
+EOF
+
+cat <<EOF > $HOME/.local/share/applications/rqt.desktop
+[Desktop Entry]
+Name=RQt
+GenericName=RQt
+Comment=Graphical tool for ROS 2
+Exec=rqt
+Icon=/opt/ros/${ROS_DISTRO}/share/rqt_gui/resource/rqt.svg
+Terminal=false
+X-MultipleArgs=false
+Type=Application
+StartupNotify=true
+Categories=Development;Robotics;
+EOF
